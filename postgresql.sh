e4f067765e99cfedc853fcdcf85f6798e933acfe
Thorsten Bruhns <thorsten.bruhns@googlemail.com>
2019-07-24 07:47:52 +0200

    refactoring postgresql.sh

--- postgresql.sh
+++ postgresql.sh
@@ -1,2 +1,14 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ] ; then
+    echo "start with path to inventory file"
+    echo "example: postgresql.sh inventory/vagrant/hosts"
+    exit 1
+fi
+
 sudo ansible-galaxy -p /etc/ansible/roles install geerlingguy.postgresql
+ansible pgserver -i ${1} -m ping
+if [ "$?" -ne 0 ] ; then
+    exit 1
+fi
 ansible-playbook -i ${1} postgres-community.yml
