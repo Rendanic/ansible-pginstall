327f276080f6b0e967797151ce9a573438d3cd53
Thorsten Bruhns <thorsten.bruhns@googlemail.com>
2020-06-24 07:45:22 +0200

    playbook rework - wip

--- postgresql.sh
+++ postgresql.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ] ; then
+if [ "$#" = 0 ] ; then
     echo "start with path to inventory file"
-    echo "example: postgresql.sh inventory/vagrant/hosts"
+    echo "example: postgresql.sh -i inventory/vagrant/hosts"
     exit 1
 fi
-
-inventory="$1"
 
 echo "ansible-galaxy geerlingguy.postgresql"
 ansible-galaxy install geerlingguy.postgresql || exit 1
 
-echo "ansible ping hosts from inventory"
-ansible pgserver -i "${inventory}" -m ping || exit 1
-
 echo "ansible-playbook"
-ansible-playbook -i "${inventory}" postgres-community.yml
+ansible-playbook postgres-community.yml $*
